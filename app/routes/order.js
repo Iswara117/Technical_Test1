f9b30ceab862408d43dad1ddcae3c8807c84583b
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2023-03-18 01:40:39 +0700

    new : add new router for path update by leader

--- app/routes/order.js
+++ app/routes/order.js
@@ -2,8 +2,9 @@ const router = require("express").Router()
 const order = require('../controllers/orders.controllers')
 const auth = require('../../middlewares/auth')
 const isApproval = require('../../middlewares/isApproval')
+const isLeader = require('../../middlewares/isLeader')
 
 router.post("/add-order", auth, order.addOrder)
 router.put("/approval/update-order/:id",   auth ,isApproval,order.updateStatusOrder1)
-
+router.put("/leader/update-order/:id",   auth ,isLeader,order.updateStatusOrder2)
 module.exports = router;
